b9b73711cde898908ce85246875c92c2d8525435
Nicolas Degen <ndegen@me.com>
2025-03-19 15:50:00 -0400

    Footer link to nide.ch

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -63,6 +63,17 @@ export default function Footer() {
             <p className="ml-2 h-7">in</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.nide.ch"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">nide</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} Echo Labs AG
